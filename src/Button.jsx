0cc1686978a472256164aed87812489638850466
R D <rusirud49@gmail.com>
2026-02-05 15:59:09 +0530

    learn useEffest & useReducer

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,18 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 
 function Button({ countToDisplay, buttonName }) {
-    const [count, setCount] = useState(0)
+    //const [count, setCount] = useState(0)
+
+    const reducer = (state, action) => {
+        if (action.type == "increment") {
+            return { count: state.count + 1 }
+        }
+        else {
+            return { count: state.count - 1 }
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
 
     useEffect(() => {
-        countToDisplay(count)
-    }, [count, countToDisplay])
+        countToDisplay(state.count)
+    }, [state.count])
+
+    useEffect(() => {
+        console.log("component is loaded")
+    }, [])
+
+    useEffect(() => {
+        return console.log("component is unloaded")
+    }, [])
 
     return (
-        <button onClick={() => setCount((count) => count + 1)}>
-            {buttonName}
-        </button>
+        <>
+            <button onClick={() => dispatch({ type: "increment" })}>
+                increment
+            </button>
+
+            <button onClick={() => dispatch({ type: "decrement" })}>
+                decrement
+            </button>
+        </>
+
     )
 }
+
 
 export default Button
 
